6c1563fcc421ffa86b9f05f82c8a3398570e9af5
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-09 12:46:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-function readFileAsync(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(`Error reading file ${filePath}: ${err}`);
-        return;
-      }
-      resolve(data);
-    });
-  });
+async function modifyText(text) {
+  return text.toUpperCase().split('').reverse().join('');
 }
 
-function modifyText(text) {
-  return new Promise((resolve, reject) => {
-    // Convert text to uppercase and reverse it
-    const upperCaseText = text.toUpperCase();
-    const reversedText = upperCaseText.split('').reverse().join('');
-    resolve(reversedText);
-  });
+async function main(filePath) {
+  try {
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    const modified = await modifyText(data);
+    console.log(modified);
+  } catch (error) {
+    console.error(`Error reading file ${filePath}: ${error.message}`);
+    process.exit(1);
+  }
 }
 
-readFileAsync(filePath)
-  .then((data) => modifyText(data))
-  .then((modifiedText) => {
-    console.log(modifiedText);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+if (require.main === module) {
+  const filePath = process.argv[2];
+  if (!filePath) {
+    console.error("Please provide a file path.");
+    process.exit(1);
+  }
+  main(filePath);
+}
